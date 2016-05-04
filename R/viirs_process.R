29c25f884adc77f49d8e79a6664d74bc7bc8e9b9
Max Joseph <maxwellbjoseph@gmail.com>
2016-05-04 11:29:29 -0600

    rasterizing colorado water data prior to masking to increase speed a bit

--- R/viirs_process.R
+++ R/viirs_process.R
@@ -10,6 +10,7 @@ viirs <- raster('output/geotiffs/viirs_ndvi_raw.tif')
 # load water polygons & match projections
 water <- readOGR('data/hydrology', 'NHDWaterbody')
 water <- spTransform(water, projection(viirs))
+water <- rasterize(water, viirs)
 
 # match extent and punch out pixels in water bodies
 viirs <- mask(viirs, water, inverse = TRUE)
